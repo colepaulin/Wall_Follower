3f01b4d2658e2f8092a508f9007ec74b0fdac792
Cole <cpaulin@mit.edu>
2023-02-26 23:07:42 +0000

    rough draft

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -29,24 +29,63 @@ class WallFollower:
     # TODO:
     # Write your callback functions here.\
 
-    def AckermannInstance_thenpublish(self,steering_angle_tp,steering_angle_velocity_tp,speed_tp,acceleration_tp,jerk_tp):
+    def Scan_Slice(self,scan,angles):
+
+        half_length = int(len(scan.ranges))/2
+        if self.SIDE == -1:
+            sliced_range = scan.ranges[half_length:]
+            sliced_angles = angles[half_length:]
+        if self.SIDE == 1:
+            sliced_range = scan.ranges[:half_length]
+            sliced_angles = angles[:half_length]
+        return sliced_range,sliced_angles
+
+    def datapoint_angles(self,scan):
+        increment = scan.angle_increment
+        angle_min = scan.angle_min
+        angles = [angle_min]
+        for i in range(1,len(scan.ranges)):
+            prev = angles[-1]
+            angles.append(prev + increment)
+        return np.array(angles)
+
+
+    def find_the_wall(self,sliced_range,sliced_angles):
+        x_dist_ranges = np.cos(sliced_angles)*sliced_range
+        y_dist_ranges = np.sin(sliced_angles)*sliced_range
+        # x_squared = np.square(x_dist_ranges)
+        # y_squared = np.square(y_dist_ranges)
+        #
+        # distances = np.sqrt(x_squared+y_squared)
+        # wall_distance = np.mean(distances)
+        [theta, wall_distance] = np.polyfit(x_dist_ranges,y_dist_ranges,1)
+        return theta, wall_distance
+
+    def control(self,theta,wall_distance):
+        error = self.DESIRED_DISTANCE - wall_distance
+        Kp = 5
+        Kd = 1
+        control_action = Kp*error + Kd*self.VELOCITY*theta
+        return control_action
+
+
+    def callback(self,scan):
+        angles = self.datapoint_angles(scan)
+        sliced_range,sliced_angles = self.Scan_Slice(scan,angles) # uses either the left or right side of the data
+
+        (theta,wall_distance) = self.find_the_wall(sliced_range,sliced_angles)
+        control_action = self.control(theta,wall_distance)
+
+
         drive_stamp = AckermannDriveStamped()
         drive_stamp.header.stamp = rospy.Time.now()
         drive_stamp.header.frame_id = "wall_follower"
-        drive_stamp.drive.steering_angle = steering_angle_tp
-        drive_stamp.drive.steering_angle_velocity = steering_angle_velocity_tp
-        drive_stamp.drive.speed = speed_tp
-        drive_stamp.drive.acceleration = acceleration_tp
-        drive_stamp.drive.jerk = jerk_tp
+        drive_stamp.drive.steering_angle = control_action
+        drive_stamp.drive.steering_angle_velocity = 0
+        drive_stamp.drive.speed = self.VELOCITY
+        drive_stamp.drive.acceleration = 0
+        drive_stamp.drive.jerk = 0
         self.drive_pub.publish(drive_stamp)
-
-    def Scan_Slice(self,):
-
-    def callback(self,scan):
-        
-
-        self.AckermannInstance_thenpublish(0,1,2,0,0)
-
 
 
 
