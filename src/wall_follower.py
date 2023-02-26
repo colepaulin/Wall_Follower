e231bf6cfe2a088a37bb6c6509970ec9ef49a75f
Cole <cpaulin@mit.edu>
2023-02-26 20:03:55 +0000

    first commit

--- src/wall_follower.py
+++ src/wall_follower.py
@@ -21,11 +21,34 @@ class WallFollower:
         # TODO:
         # Initialize your publishers and
         # subscribers here
+        self.drive_pub = rospy.Publisher(self.DRIVE_TOPIC,AckermannDriveStamped, queue_size = 10)
+        self.laser_sub = rospy.Subscriber(self.SCAN_TOPIC, numpy_msg(LaserScan), self.callback)
 
-        pass
+
 
     # TODO:
-    # Write your callback functions here.
+    # Write your callback functions here.\
+
+    def AckermannInstance_thenpublish(self,steering_angle_tp,steering_angle_velocity_tp,speed_tp,acceleration_tp,jerk_tp):
+        drive_stamp = AckermannDriveStamped()
+        drive_stamp.header.stamp = rospy.Time.now()
+        drive_stamp.header.frame_id = "wall_follower"
+        drive_stamp.drive.steering_angle = steering_angle_tp
+        drive_stamp.drive.steering_angle_velocity = steering_angle_velocity_tp
+        drive_stamp.drive.speed = speed_tp
+        drive_stamp.drive.acceleration = acceleration_tp
+        drive_stamp.drive.jerk = jerk_tp
+        self.drive_pub.publish(drive_stamp)
+
+    def Scan_Slice(self,):
+
+    def callback(self,scan):
+        
+
+        self.AckermannInstance_thenpublish(0,1,2,0,0)
+
+
+
 
 if __name__ == "__main__":
     rospy.init_node('wall_follower')
